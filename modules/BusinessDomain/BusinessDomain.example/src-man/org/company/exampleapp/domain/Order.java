cd50540401cefc1ec6eda7dd6358daeb34c39e6e
johan <johan@86619b79-1f46-0410-a122-eb9e176381dc>
2008-04-08 08:40:45 +0000

    Added extends of implBase classes

---
+++ modules/BusinessDomain/BusinessDomain.example/src-man/org/company/exampleapp/domain/Order.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.company.exampleapp.domain;
+
+/**
+ * @author johan
+ *
+ */
+public class Order extends OrderImplBase {
+
+	/**
+	 * 
+	 */
+	public Order() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ modules/BusinessDomain/BusinessDomain.example/src-man/org/company/exampleapp/domain/Person.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.company.exampleapp.domain;
+
+/**
+ * @author johan
+ *
+ */
+public class Person extends PersonImplBase {
+
+	/**
+	 * 
+	 */
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
