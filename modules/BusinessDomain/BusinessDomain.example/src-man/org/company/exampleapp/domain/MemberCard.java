4ee81170d80f2d1ea9ceeb80ab064a51559a93e5
johan <johan@86619b79-1f46-0410-a122-eb9e176381dc>
2008-04-17 12:20:35 +0000

    Added membercard

---
+++ modules/BusinessDomain/BusinessDomain.example/src-man/org/company/exampleapp/domain/MemberCard.java
@@ -0,0 +1,15 @@
+package org.company.exampleapp.domain;
+
+/**
+ * Extension point initially generated at: Thu Apr 17 14:20:04 CEST 2008
+ */
+public class MemberCard extends MemberCardImplBase {
+
+	/**
+	 *  Constructor for MemberCard 
+	 */
+	public MemberCard(int cardNumber) {
+		super(cardNumber);
+	}
+
+}
