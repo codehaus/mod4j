0670e8d2077d724f14512d3a7973e9f8c252136c
johan <johan@86619b79-1f46-0410-a122-eb9e176381dc>
2008-04-22 08:25:37 +0000

    Initial version of the BusinessDomain.example.Xtext project

---
+++ modules/BusinessDomain/BusinessDomain.example.Xtext/src-man/org/company/exampleappXtext/domain/Person.java
@@ -0,0 +1,26 @@
+
+			package org.company.exampleappXtext.domain;
+				
+			/**
+			* Extension point initially generated at: Tue Apr 22 10:20:29 CEST 2008
+			*/
+        	public class Person extends PersonImplBase {
+            	
+	/**
+	 *  Minimal constructor for Person
+ 	
+ 		* @param is () 
+ 		* @param firstName () 
+ 		* @param lastName () 
+ 	
+     */
+     public Person ( 	
+ 		     is 	, 		     firstName 	, 		     lastName 	
+) {
+         super(	
+is,firstName,lastName
+);
+ 	 }
+
+ 			}	
+		
---
+++ modules/BusinessDomain/BusinessDomain.example.Xtext/src-man/org/company/exampleappXtext/domain/Person2.java
@@ -0,0 +1,21 @@
+package org.company.exampleappXtext.domain;
+
+/**
+ * Extension point initially generated at: Tue Apr 22 10:20:29 CEST 2008
+ */
+public class Person2 extends Person2ImplBase {
+
+	/**
+	 *  Minimal constructor for Person2
+	
+	
+	 */
+	public Person2(
+
+	) {
+		super(
+
+		);
+	}
+
+}
