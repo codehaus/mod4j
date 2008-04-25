ae3925c9ac0b49d53771f373be20fdf81f669385
johan <johan@86619b79-1f46-0410-a122-eb9e176381dc>
2008-04-25 13:03:22 +0000

    Added RecordShop example

---
+++ modules/IntegralExample/RecordShop-Application/RecordShop-domain/src/main/java/org/company/recordshop/domain/MemberCard.java
@@ -0,0 +1,18 @@
+package org.company.recordshop.domain;
+
+/**
+ * Extension point initially generated at: Fri Apr 25 14:52:55 CEST 2008
+ */
+public class MemberCard extends MemberCardImplBase {
+
+	/**
+	 *  Minimal constructor for MemberCard
+	
+	 * @param cardNumber (int) The unique number identifying this card
+	
+	 */
+	public MemberCard(int cardNumber) {
+		super(cardNumber);
+	}
+
+}
---
+++ modules/IntegralExample/RecordShop-Application/RecordShop-domain/src/main/java/org/company/recordshop/domain/Person.java
@@ -0,0 +1,19 @@
+package org.company.recordshop.domain;
+
+/**
+ * Extension point initially generated at: Fri Apr 25 14:52:55 CEST 2008
+ */
+public class Person extends PersonImplBase {
+
+	/**
+	 *  Minimal constructor for Person
+	
+	 * @param lastName (String) 
+	 * @param firstName (String) 
+	
+	 */
+	public Person(String lastName, String firstName) {
+		super(lastName, firstName);
+	}
+
+}
