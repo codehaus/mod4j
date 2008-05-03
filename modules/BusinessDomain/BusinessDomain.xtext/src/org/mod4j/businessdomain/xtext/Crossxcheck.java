108e1080518c92cb7f970617e89b3d26feb33b0b
jwa11799 <jwa11799@86619b79-1f46-0410-a122-eb9e176381dc>
2008-05-03 17:05:23 +0000

    Util Java operation for use in Checks

---
+++ modules/BusinessDomain/BusinessDomain.xtext/src/org/mod4j/businessdomain/xtext/Crossxcheck.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Ordina and committers to Mod4j
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Ordina - initial implementation
+ *******************************************************************************/
+package org.mod4j.businessdomain.xtext;
+
+import java.io.File;
+import java.util.Date;
+
+import mod4j.crossx.broker.repository.*;
+
+public class Crossxcheck {
+
+	/** Creates a timestamp of the current moment
+	 * 
+	 * @return Current time as a String
+	 */
+    public static String timestamp() {
+        Date now = new Date(System.currentTimeMillis());
+        return now.toString();
+    }
+    /**
+     * @param path
+     * @return True if the file named path exists, otherwise false
+     */
+    public static boolean fileExist(String path){
+        File f = new File(path);
+        return f.exists();
+     }
+
+    /**
+     * @param path
+     * @return Returns true of a BusinessClass with the name path exists in any model in the workspace
+     */
+    public static boolean classExists(String path){
+    	String resource = CrossxRepository.find(path, "BusinessClass") ;
+    	return (resource != null);
+    }
+
+    /** This does the sama as classExists, need a way to not do this twice
+     *   Returns the name of the resource in which the BusinessClass path is defined.
+     */
+    public static String definedInResource(String path){
+    	String result = CrossxRepository.find(path, "BusinessClass") ;
+    	return result ;
+    }
+}
