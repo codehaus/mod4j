ebfd65306f302ee19c9b894abba8f115fd8c35d1
johan <johan@86619b79-1f46-0410-a122-eb9e176381dc>
2008-05-09 19:11:35 +0000

    New generated

---
+++ modules/IntegralExample/RecordShop-Application/RecordShop-domain/src/main/java/org/company/recordshop/domain/MemberCard.java
@@ -0,0 +1,20 @@
+package org.company.recordshop.domain;
+
+import org.joda.time.*;
+
+/**
+ * Extension point initially generated at: Fri May 09 21:09:26 CEST 2008
+ */
+public class MemberCard extends MemberCardImplBase {
+
+	/**
+	 *  Minimal constructor for MemberCard
+	
+	 * @param cardNumber (int) The unique number identifying this card
+	
+	 */
+	public MemberCard(int cardNumber) {
+		super(cardNumber);
+	}
+
+}
---
+++ modules/IntegralExample/RecordShop-Application/RecordShop-domain/src/main/java/org/company/recordshop/domain/Person.java
@@ -0,0 +1,19 @@
+package org.company.recordshop.domain;
+
+/**
+ * Extension point initially generated at: Fri May 09 21:09:26 CEST 2008
+ */
+public class Person extends PersonImplBase {
+
+	/**
+	 *  Minimal constructor for Person
+	
+	 * @param lastName (String) 
+	 * @param firstName (String) 
+	
+	 */
+	public Person(String lastName, String firstName) {
+		super(lastName, firstName);
+	}
+
+}
