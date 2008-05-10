4c59dd67c202bc2551802d22e06dba582e67f719
johan <johan@86619b79-1f46-0410-a122-eb9e176381dc>
2008-05-10 18:56:26 +0000

    Added new Xtext model for RecordShop

---
+++ modules/IntegralExample/RecordShop-Application/RecordShop-domain/src/main/java/org/company/recordshop/domain/OrderLine.java
@@ -0,0 +1,21 @@
+package org.company.recordshop.domain;
+
+/**
+ * Extension point initially generated at: Sat May 10 20:55:16 CEST 2008
+ */
+public class OrderLine extends OrderLineImplBase {
+
+	/**
+	 *  Minimal constructor for OrderLine
+	
+	 * @param lineNumber (int) 
+	 * @param description (String) 
+	 * @param lineAmount (float) 
+	
+	 */
+	public OrderLine(int lineNumber, String description, float lineAmount) {
+
+		super(lineNumber, description, lineAmount);
+	}
+
+}
---
+++ modules/IntegralExample/RecordShop-Application/RecordShop-domain/src/main/java/org/company/recordshop/domain/Person.java
@@ -0,0 +1,21 @@
+package org.company.recordshop.domain;
+
+/**
+ * Extension point initially generated at: Sat May 10 20:55:15 CEST 2008
+ */
+public class Person extends PersonImplBase {
+
+	/**
+	 *  Minimal constructor for Person
+	
+	 * @param firstName (String) 
+	 * @param lastName (String) 
+	 * @param numberOfEars (int) 
+	
+	 */
+	public Person(String firstName, String lastName, int numberOfEars) {
+
+		super(firstName, lastName, numberOfEars);
+	}
+
+}
