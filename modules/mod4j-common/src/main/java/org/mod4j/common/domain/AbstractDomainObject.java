224a949840b793760a5e91d54b771daf7829506c
emalotau <emalotau@86619b79-1f46-0410-a122-eb9e176381dc>
2008-05-14 17:50:22 +0000

    Added Javadoc to the methods of AbstractDomainObject.java.

--- modules/mod4j-common/src/main/java/org/mod4j/common/domain/AbstractDomainObject.java
+++ modules/mod4j-common/src/main/java/org/mod4j/common/domain/AbstractDomainObject.java
@@ -1,8 +1,10 @@
 package org.mod4j.common.domain;
 
+import java.net.BindException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.mod4j.common.exception.BusinessRuleException;
 import org.mod4j.common.validation.BusinessRuleValidationTemplate;
 import org.springframework.validation.Validator;
 
@@ -18,13 +20,32 @@ public abstract class AbstractDomainObject {
     protected final BusinessRuleValidationTemplate validationTemplate = new BusinessRuleValidationTemplate(
             this);
 
+    /**
+     * Add the given <code>validator</code> to the collection of validators
+     * that will be executed on this object when the {@link #validate()} method
+     * is called. The validators will be invoked in the order they were added.
+     * 
+     * @param validator -
+     *            the validator being added.
+     */
     public void addValidator(Validator validator) {
         if (!validators.contains(validator)) {
             validators.add(validator);
         }
     }
 
-    protected void validate() throws RuntimeException {
+    /**
+     * Validate this object.
+     * 
+     * Validation is performed by calling every {@link Validator} added by the
+     * {@link #addValidator(Validator)} method, in the order they were added.
+     * 
+     * @throws BusinessRuleException -
+     *             thrown when one or more errors are found. The exception
+     *             thrown has as its cause a {@link BindException} containing
+     *             information on all errors found.
+     */
+    protected void validate() throws BusinessRuleException {
         validationTemplate.invokeValidators(validators);
     }
 
