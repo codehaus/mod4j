ed810657702b69690b50567c79e02962957c0c53
jwa11799 <jwa11799@86619b79-1f46-0410-a122-eb9e176381dc>
2008-06-19 07:55:08 +0000

    Added code (copied from Eclipse) to create project at different location.

--- modules/BusinessDomain/BusinessDomain.xtext.editor/src/org/mod4j/businessdomain/xtext/wizards/Mod4jAbstractNewProjectWizard.java
+++ modules/BusinessDomain/BusinessDomain.xtext.editor/src/org/mod4j/businessdomain/xtext/wizards/Mod4jAbstractNewProjectWizard.java
@@ -11,17 +11,35 @@ package org.mod4j.businessdomain.xtext.wizards;
  *******************************************************************************/
 
 import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
 
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResourceStatus;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.WorkspaceModifyOperation;
+import org.eclipse.ui.ide.undo.CreateProjectOperation;
+import org.eclipse.ui.ide.undo.WorkspaceUndoUtil;
+import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.eclipse.ui.internal.ide.StatusUtil;
+import org.eclipse.ui.internal.wizards.newresource.ResourceMessages;
+import org.eclipse.ui.statushandlers.StatusAdapter;
+import org.eclipse.ui.statushandlers.StatusManager;
 import org.openarchitectureware.xtext.LanguageUtilities;
 import org.openarchitectureware.xtext.XtextLog;
 // import org.openarchitectureware.xtext.editor.wizards.ProjectCreator;
@@ -95,8 +113,9 @@ public abstract class Mod4jAbstractNewProjectWizard extends Wizard implements
 					protected void execute(IProgressMonitor monitor) throws CoreException, InvocationTargetException,
 							InterruptedException {
 						try {
-							IProject pr = mainPage.getProjectHandle();
-							pr.create(monitor);
+//							IProject pr = mainPage.getProjectHandle();
+//							pr.create(monitor);
+							IProject pr = createNewProject();
 							pr.open(monitor);
 							String modelFileName = "model." + getFileExtension();
 //							Mod4jProjectCreator.create(pr, new String[] { getDslProjectName(), getGeneratorProjectName() }, monitor);
@@ -323,6 +342,116 @@ public abstract class Mod4jAbstractNewProjectWizard extends Wizard implements
 			"</workflow>" + LF;
 		}
 		
-		
+		/**
+		 * Creates a new project resource with the selected name.
+		 * <p>
+		 * In normal usage, this method is invoked after the user has pressed Finish
+		 * on the wizard; the enablement of the Finish button implies that all
+		 * controls on the pages currently contain valid values.
+		 * </p>
+		 * <p>
+		 * Note that this wizard caches the new project once it has been
+		 * successfully created; subsequent invocations of this method will answer
+		 * the same project resource without attempting to create it again.
+		 * </p>
+		 * 
+		 * @return the created project resource, or <code>null</code> if the
+		 *         project was not created
+		 */
+		private IProject createNewProject() {
+			IProject newProject = null;
+			if (newProject != null) {
+				return newProject;
+			}
+
+			// get a project handle
+			final IProject newProjectHandle = mainPage.getProjectHandle();
+
+			// get a project descriptor
+			URI location = null;
+			if (!mainPage.useDefaults()) {
+				location = mainPage.getLocationURI();
+			}
+
+			IWorkspace workspace = ResourcesPlugin.getWorkspace();
+			final IProjectDescription description = workspace
+					.newProjectDescription(newProjectHandle.getName());
+			description.setLocationURI(location);
+
+			// update the referenced project if provided
+//			if (referencePage != null) {
+//				IProject[] refProjects = referencePage.getReferencedProjects();
+//				if (refProjects.length > 0) {
+//					description.setReferencedProjects(refProjects);
+//				}
+//			}
+
+			// create the new project operation
+			IRunnableWithProgress op = new IRunnableWithProgress() {
+				public void run(IProgressMonitor monitor)
+						throws InvocationTargetException {
+					CreateProjectOperation op = new CreateProjectOperation(
+							description, ResourceMessages.NewProject_windowTitle);
+					try {
+						PlatformUI.getWorkbench().getOperationSupport()
+								.getOperationHistory().execute(
+										op,
+										monitor,
+										WorkspaceUndoUtil
+												.getUIInfoAdapter(getShell()));
+					} catch (ExecutionException e) {
+						throw new InvocationTargetException(e);
+					}
+				}
+			};
+
+			// run the new project creation operation
+			try {
+				getContainer().run(false, true, op);
+			} catch (InterruptedException e) {
+				return null;
+			} catch (InvocationTargetException e) {
+				Throwable t = e.getTargetException();
+				if (t instanceof ExecutionException
+						&& t.getCause() instanceof CoreException) {
+					CoreException cause = (CoreException) t.getCause();
+					StatusAdapter status;
+					if (cause.getStatus().getCode() == IResourceStatus.CASE_VARIANT_EXISTS) {
+						status = new StatusAdapter(
+								StatusUtil
+										.newStatus(
+												IStatus.WARNING,
+												NLS
+														.bind(
+																ResourceMessages.NewProject_caseVariantExistsError,
+																newProjectHandle
+																		.getName()),
+												cause));
+					} else {
+						status = new StatusAdapter(StatusUtil.newStatus(cause
+								.getStatus().getSeverity(),
+								ResourceMessages.NewProject_errorMessage, cause));
+					}
+					status.setProperty(StatusAdapter.TITLE_PROPERTY,
+							ResourceMessages.NewProject_errorMessage);
+					StatusManager.getManager().handle(status, StatusManager.BLOCK);
+				} else {
+					StatusAdapter status = new StatusAdapter(new Status(
+							IStatus.WARNING, IDEWorkbenchPlugin.IDE_WORKBENCH, 0,
+							NLS.bind(ResourceMessages.NewProject_internalError, t
+									.getMessage()), t));
+					status.setProperty(StatusAdapter.TITLE_PROPERTY,
+							ResourceMessages.NewProject_errorMessage);
+					StatusManager.getManager().handle(status,
+							StatusManager.LOG | StatusManager.BLOCK);
+				}
+				return null;
+			}
+
+			newProject = newProjectHandle;
+
+			return newProject;
+		}
+
 
 	}
