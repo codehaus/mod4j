3a3410c3fadd8db00c5e144596590079d4ac60ab
johan <johan@86619b79-1f46-0410-a122-eb9e176381dc>
2008-07-03 18:44:12 +0000

    Added javadoc

--- modules/BusinessDomain/BusinessDomain.generator/src/main/java/org/mod4j/businessdomain/generator/helpers/BusinessRuleHelpers.java
+++ modules/BusinessDomain/BusinessDomain.generator/src/main/java/org/mod4j/businessdomain/generator/helpers/BusinessRuleHelpers.java
@@ -8,21 +8,25 @@ import BusinessDomainDsl.BusinessRule;
 public class BusinessRuleHelpers {
 
     /**
-     * @param cls
-     * @return The name of the Java class for cls
+     * @param br
+     * @return The name of the Java class for br
      */
     public static String javaClassName(BusinessRule br) {
         return StringHelpers.firstCharToUpper(br.getName());
     }
 
     /**
-     * @param cls
+     * @param br
      * @return The name of the ImplBase class for BusinessRule br
      */
     public static String implBaseClassName(BusinessRule br) {
         return javaClassName(br) + ProjectProperties.IMPLBASE_SUFFIX;
     }
 
+    /**
+     * 
+     * @return The full packagename of business rules
+     */
     public static String getBusinessRulesPackage() {
         return ProjectProperties.getDomainRootPackage() + "." + ProjectProperties.BUSINESSRULES_PACKAGE;
     }
