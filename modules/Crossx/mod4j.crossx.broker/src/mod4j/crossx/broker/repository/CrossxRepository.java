3b79ac43df836f98adfd98aa0498da4326028fc9
jwa11799 <jwa11799@86619b79-1f46-0410-a122-eb9e176381dc>
2008-07-29 08:45:18 +0000

    First working version

--- modules/Crossx/mod4j.crossx.broker/src/mod4j/crossx/broker/repository/CrossxRepository.java
+++ modules/Crossx/mod4j.crossx.broker/src/mod4j/crossx/broker/repository/CrossxRepository.java
@@ -1,22 +1,38 @@
 package mod4j.crossx.broker.repository;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.eclipse.ui.console.MessageConsoleStream;
 import org.jdom.Document;
 import org.jdom.Element;
+
+import crossx.util.EclipseUtil;
 
 public class CrossxRepository {
 
 	static private List<Element> info = new ArrayList<Element>();
 	static private Document all = new Document(new Element("ROOT"));
 
+//	   public static String getTimestamp() {
+//	        Date now = new Date(System.currentTimeMillis());
+//	        return now.toString();
+//	        
+//	    }
+
 	static public Element getAll(){
 		return all.getRootElement();
 	}
 	
+	/** Add all the XML elements in 'doc' to the repository.
+	 *  If the repository already contains information from the same resource, remove
+	 *  the old information
+	 *  
+	 * @param doc The XML document to add
+	 */
 	static public void addInfo(Document doc) {
-		System.out.println("Repository add [" + doc.getRootElement().toString());
+		print("Repository add [" + doc.getRootElement().toString() + "]");
 		Element toRemove = null;
 
 		Element newInfo     = doc.getRootElement();
@@ -25,7 +41,7 @@ public class CrossxRepository {
 			String resource = modelinfo.getChild("ModelReference").getAttributeValue("resource");
 			if( resource.equals(newResource) ){
 				toRemove = modelinfo;
-				System.out.println("CrossxRepository: remove [" + resource + "]");
+				print("CrossxRepository: remove [" + resource + "]");
 			}
 		}
 		
@@ -41,18 +57,29 @@ public class CrossxRepository {
 		printXML();
 	}
 	
-	static void printXML() {
-		System.out.println("XML [" + all.getRootElement().getName()+ "]");
+	public static void printXML() {
+		print("XML [" + all.getRootElement().getName()+ "]");
 		for( Object o : all.getRootElement().getChildren()) {
 			Element e = (Element)o;
-			System.out.println("    XML [" + e.getName() + "]");
+			print("    XML [" + e.getName() + "]");
+			for( Object ch : e.getChildren()) {
+				Element child = (Element)ch;
+				print("    XML [" + child.getAttributeValue("name") + "]");
+			}
+
 		}
 	}
+	
 	static public String find(String name, String elemType) {
+		checkCrossxStatus();
+		print("CROSSX find: [" + CrossxRepository.class.getClassLoader().toString() + "]");
+		print("CROSSX find: [" + name + "] [" + elemType + "]");
 		for(Element modelinfo : info){
+			print("CROSSX FIND modelinfo");
 			for(Object elem : modelinfo.getChildren() ){
 				if( elem instanceof Element) {
 					Element e = (Element) elem;
+					print("CROSSX FIND child [" + elem.toString() + "]");
 					if( e.getAttributeValue("name"    ).equals(name) && 
 						e.getAttributeValue("elemType").equals(elemType) )
 					{
@@ -65,5 +92,49 @@ public class CrossxRepository {
 		}
 		return null;
 	}
+
+	static public List<String> findAll(String elemType) {
+		checkCrossxStatus();
+		print("CROSSX findAll: [" + CrossxRepository.class.getClassLoader().toString() + "]");
+		print("CROSSX findAll: [" + elemType + "]");
+		List<String> result = new ArrayList<String>();
+		for(Element modelinfo : info){
+			print("CROSSX modelinfo");
+			for(Object elem : modelinfo.getChildren() ){
+				print("CROSSX child [" + elem.toString() + "]");
+				if( elem instanceof Element) {
+					Element e = (Element) elem;
+					print("CROSSX element [" + e.getName() + "]");
+					String value = e.getAttributeValue("elemType");
+					if( (value != null) && value.equals(elemType) )
+					{
+						print("CROSSX found: [" + e.getAttributeValue("name") + "]");
+						result.add(e.getAttributeValue("name"));
+					} else {
+						print("CROSSX NOT found: [" + e.getAttributeValue("name") + "]");						
+					}
+				}
+			}
+		}
+		return result;
+	}
+
+	static boolean first = true;
 	
+	private static void checkCrossxStatus() {
+		print("checkCrossxStatus");
+		if( first ){
+			first = false;
+		} else {
+			return;
+		}
+	}
+	private static MessageConsoleStream console;
+
+	private static void print(String text){
+		if( console == null ){
+			console= EclipseUtil.findConsole("crossx.console");
+		}
+		console.println(text);
+	}
 }
