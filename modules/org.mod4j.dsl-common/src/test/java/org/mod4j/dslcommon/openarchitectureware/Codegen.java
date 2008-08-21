bb9e28b63cbb487758e358d573c794906d66f1fb
jwa11799 <jwa11799@86619b79-1f46-0410-a122-eb9e176381dc>
2008-08-21 12:26:57 +0000

    Call walker with src/model as start

--- modules/org.mod4j.dsl-common/src/test/java/org/mod4j/dslcommon/openarchitectureware/Codegen.java
+++ modules/org.mod4j.dsl-common/src/test/java/org/mod4j/dslcommon/openarchitectureware/Codegen.java
@@ -4,34 +4,33 @@ import org.mod4j.dslcommon.io.DirectoryWalker;
 
 public class Codegen {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		DslExtension dsl = new DslExtension("BusinessDomainDsl", 
-									"BusinessDomainDsl", 
-									"BusinessDomainDsl.BusinessDomainDslPackage", 
-									".busmod", 
-									"crossx/busmod2crossx2.oaw", 
-									"src/workflow/busmod.oaw", 
-									"src/workflow/busmod.properties");
-		
-		String dir = "D:/mod4j/repository/modules/RecordShop-ExampleProject/RecordShop.BusinessDomain.model";
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        DslExtension dsl = new DslExtension("BusinessDomainDsl", "BusinessDomainDsl",
+                "BusinessDomainDsl.BusinessDomainDslPackage", ".busmod", "crossx/busmod2crossx2.oaw",
+                "codegen/BusinessDomainDsl.oaw", "BusinessDomainDsl.properties");
 
-		execute(dir, dsl);
-	}
-	public static void execute(String projectDir, DslExtension dsl) {
-		DirectoryWalker walker = new DirectoryWalker();
-		CrossxDirectoryVisitor vis = new CrossxDirectoryVisitor(dsl, projectDir);
-		try {
-            walker.walk(projectDir, vis	);
+        String dir = "D:/mod4j/repository/modules/RecordShop-ExampleProject/RecordShop.BusinessDomain.model";
+
+        execute(dir, dsl);
+    }
+
+    private static String MODEL_DIR = "src/model";
+
+    public static void execute(String projectDir, DslExtension dsl) {
+        DirectoryWalker walker = new DirectoryWalker();
+        CrossxDirectoryVisitor vis = new CrossxDirectoryVisitor(dsl, projectDir);
+        try {
+            walker.walk(projectDir + "/" + MODEL_DIR, vis);
             CodegenDirectoryVisitor codegen = new CodegenDirectoryVisitor(dsl, projectDir);
-            walker.walk(projectDir, codegen);
+            walker.walk(projectDir + "/" + MODEL_DIR, codegen);
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-	}
+    }
 
 }
