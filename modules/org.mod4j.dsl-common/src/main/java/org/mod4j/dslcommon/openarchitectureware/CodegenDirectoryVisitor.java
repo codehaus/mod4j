ead4636aae02f5bf2d4122ea0e365ffce1866896
johan <johan@86619b79-1f46-0410-a122-eb9e176381dc>
2008-08-22 12:23:41 +0000

    Format

--- modules/org.mod4j.dsl-common/src/main/java/org/mod4j/dslcommon/openarchitectureware/CodegenDirectoryVisitor.java
+++ modules/org.mod4j.dsl-common/src/main/java/org/mod4j/dslcommon/openarchitectureware/CodegenDirectoryVisitor.java
@@ -12,96 +12,104 @@ import org.mod4j.dslcommon.io.IDirectoryVisitor;
  * @author jos
  */
 public class CodegenDirectoryVisitor implements IDirectoryVisitor {
-	
-	private static final String MODEL_DIR = "src/model";
-	private DslExtension dsl = null;
-	
-	public CodegenDirectoryVisitor(DslExtension dsl, String theWorkDir){
-		this.dsl = dsl;
-		workDir = theWorkDir;
-		setupDsl();
-	}
 
-	/* (non-Javadoc)
-	 * @see org.mod4j.dslcommon.io.IDirectoryVisitor#visitDirectoryBefore(java.io.File)
-	 */
-	public Object visitDirectoryBefore(File directory) {
-		return null;
-	}
+    private static final String MODEL_DIR = "src/model";
 
-	/* (non-Javadoc)
-	 * @see org.mod4j.dslcommon.io.IDirectoryVisitor#visitDirectoryAfter(java.io.File)
-	 */
-	public Object visitDirectoryAfter(File directory) {
-		return null;
-	}
+    private DslExtension dsl = null;
 
-	/* (non-Javadoc)
-	 * @see org.mod4j.dslcommon.io.IDirectoryVisitor#visitFile(java.io.File)
-	 */
-	public Object visitFile(File file) throws Mod4jWorkflowException {
-		if( file.isFile() && file.getName().endsWith(dsl.getDslFileExtension()) )
-		{
-			generateCode(file);
-		}
-		return null;
-	}
-		
-	private String workDir = "";
-	private String propertiesFile = null;
+    public CodegenDirectoryVisitor(DslExtension dsl, String theWorkDir) {
+        this.dsl = dsl;
+        workDir = theWorkDir;
+        setupDsl();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.mod4j.dslcommon.io.IDirectoryVisitor#visitDirectoryBefore(java.io.File)
+     */
+    public Object visitDirectoryBefore(File directory) {
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.mod4j.dslcommon.io.IDirectoryVisitor#visitDirectoryAfter(java.io.File)
+     */
+    public Object visitDirectoryAfter(File directory) {
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.mod4j.dslcommon.io.IDirectoryVisitor#visitFile(java.io.File)
+     */
+    public Object visitFile(File file) throws Mod4jWorkflowException {
+        if (file.isFile() && file.getName().endsWith(dsl.getDslFileExtension())) {
+            generateCode(file);
+        }
+        return null;
+    }
+
+    private String workDir = "";
+
+    private String propertiesFile = null;
+
     private String codegenWorkflow = null;
-	private Map<String, String> properties = null;
-	
-	private void setupDsl() {
-//		propertiesFile = workDir + "/" + dsl.getDslCodegenProperties();
-		propertiesFile = workDir + "/" +  MODEL_DIR + "/" + dsl.getDslName() + ".properties";
-		File file = new File(propertiesFile);
-		if( ! file.exists() ) {
-			System.err.println("Mod4j: code generation properties file ["+ propertiesFile +
-	                "] not found, cannot generate code for dsl " + dsl.getDslName() + ".");
-			System.exit(1);
-		}
-		
-		codegenWorkflow = dsl.getDslCodegenWorkflow();
+
+    private Map<String, String> properties = null;
+
+    private void setupDsl() {
+        // propertiesFile = workDir + "/" + dsl.getDslCodegenProperties();
+        propertiesFile = workDir + "/" + MODEL_DIR + "/" + dsl.getDslName() + ".properties";
+        File file = new File(propertiesFile);
+        if (!file.exists()) {
+            System.err.println("Mod4j: code generation properties file [" + propertiesFile
+                    + "] not found, cannot generate code for dsl " + dsl.getDslName() + ".");
+            System.exit(1);
+        }
+
+        codegenWorkflow = dsl.getDslCodegenWorkflow();
         ClassLoader cls = this.getClass().getClassLoader();
         URL url = cls.getResource(codegenWorkflow);
-        if( url == null ){
+        if (url == null) {
             System.err.println("Code generation oaW file [" + codegenWorkflow + "] not found");
             System.exit(1);
         }
         codegenWorkflow = url.toString();
 
-		properties = ModelHelpers.getProperties(propertiesFile);
+        properties = ModelHelpers.getProperties(propertiesFile);
 
-		properties.put("appPropFilePath", propertiesFile);
-		
-		// Get the relative applicationPath property and make it absolute
-		String applicationPath = properties.get("applicationPath");
-		String newAppPath = workDir + "/" + applicationPath;
-		properties.put("applicationPath", newAppPath );
-		properties.put("workDir", workDir);
+        properties.put("appPropFilePath", propertiesFile);
 
-	}
-	
-	/**
-	 * @param file
-	 * @throws Mod4jWorkflowException
-	 */
-	private void generateCode(final File file) throws Mod4jWorkflowException {
-		String modelfile = file.getAbsolutePath();
-		modelfile = StringHelpers.replaceAllSubstrings(modelfile, "\\", "/");
-		modelfile = "file:/"+ modelfile;
+        // Get the relative applicationPath property and make it absolute
+        String applicationPath = properties.get("applicationPath");
+        String newAppPath = workDir + "/" + applicationPath;
+        properties.put("applicationPath", newAppPath);
+        properties.put("workDir", workDir);
+
+    }
+
+    /**
+     * @param file
+     * @throws Mod4jWorkflowException
+     */
+    private void generateCode(final File file) throws Mod4jWorkflowException {
+        String modelfile = file.getAbsolutePath();
+        modelfile = StringHelpers.replaceAllSubstrings(modelfile, "\\", "/");
+        modelfile = "file:/" + modelfile;
 
         properties.put("modelFile", modelfile);
 
-		RunGeneratorWorkflow genWf = new RunGeneratorWorkflow();
-		genWf.runWorkflow(codegenWorkflow, properties);
-	}
-	
-	public DslExtension isDslFile(String filename){
-		// TODO NEed a way to find the DslExtension info outside eclipse
-		return null;
-	}
+        RunGeneratorWorkflow genWf = new RunGeneratorWorkflow();
+        genWf.runWorkflow(codegenWorkflow, properties);
+    }
 
-	
+    public DslExtension isDslFile(String filename) {
+        // TODO NEed a way to find the DslExtension info outside eclipse
+        return null;
+    }
+
 }
