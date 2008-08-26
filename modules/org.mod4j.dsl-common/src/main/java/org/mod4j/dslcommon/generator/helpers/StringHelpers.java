93d9b93413672abe7acf6957da332ae37a98ff57
jwa11799 <jwa11799@86619b79-1f46-0410-a122-eb9e176381dc>
2008-08-26 06:21:04 +0000

    Changes layout and comments

--- modules/org.mod4j.dsl-common/src/main/java/org/mod4j/dslcommon/generator/helpers/StringHelpers.java
+++ modules/org.mod4j.dsl-common/src/main/java/org/mod4j/dslcommon/generator/helpers/StringHelpers.java
@@ -1,5 +1,13 @@
-/** (c) Copyright 2003 by Klasse Objecten
- */
+/*******************************************************************************
+ * Copyright (c) 2008 Ordina and committers to Mod4j
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jos Warmer & Anneke Kleppe - initial implementation taken from Octopus
+ *******************************************************************************/
 package org.mod4j.dslcommon.generator.helpers;
 
 /**
