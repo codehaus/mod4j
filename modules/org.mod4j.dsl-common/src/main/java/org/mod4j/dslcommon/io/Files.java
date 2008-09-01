b43e969cc93715652cc219bd4f8233aa51127204
jwa11799 <jwa11799@86619b79-1f46-0410-a122-eb9e176381dc>
2008-09-01 14:32:59 +0000

    Added javadoc

--- modules/org.mod4j.dsl-common/src/main/java/org/mod4j/dslcommon/io/Files.java
+++ modules/org.mod4j.dsl-common/src/main/java/org/mod4j/dslcommon/io/Files.java
@@ -4,11 +4,21 @@ import java.net.URL;
 
 public class Files {
     
+    /**
+     *  Try to find a file on the CLASSPATH
+     * @param filename The file to find, relatrive path to the CLASSPATH
+     * @return If found, the URL of the file, otherwise null
+     */
     public static String findFile(String filename) {
         Files f = new Files();
         return f.getDir(filename);
     }
     
+    /**
+     *  Try to find a file on the CLASSPATH
+     * @param filename The file to find
+     * @return If found, the URL of the file, otherwise null
+     */
     public String getDir(String filename){
         ClassLoader cls = this.getClass().getClassLoader();
         URL url = cls.getResource(filename);
