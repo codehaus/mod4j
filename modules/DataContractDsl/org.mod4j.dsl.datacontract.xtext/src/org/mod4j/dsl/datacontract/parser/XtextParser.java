ce73124fba81736376ad6bbad0181d298d77387b
jwa11799 <jwa11799@86619b79-1f46-0410-a122-eb9e176381dc>
2008-09-09 13:08:22 +0000

    Add generated code

---
+++ modules/DataContractDsl/org.mod4j.dsl.datacontract.xtext/src/org/mod4j/dsl/datacontract/parser/XtextParser.java
@@ -0,0 +1,12 @@
+
+package org.mod4j.dsl.datacontract.parser;
+
+import java.io.InputStream;
+
+public class XtextParser extends GenParser {
+
+	public XtextParser(InputStream in) {
+		super(in);
+	}
+
+}
