ea416f7ab3a3c96f863ff3849d24846fdb6c0650
jwa11799 <jwa11799@86619b79-1f46-0410-a122-eb9e176381dc>
2008-09-11 19:12:51 +0000

    Cross changes such that projects do not interfere with each other anymore

--- modules/BusinessDomainDsl/org.mod4j.dsl.businessdomain.generator/src/main/java/org/mod4j/businessdomain/generator/helpers/CrossxHelpers.java
+++ modules/BusinessDomainDsl/org.mod4j.dsl.businessdomain.generator/src/main/java/org/mod4j/businessdomain/generator/helpers/CrossxHelpers.java
@@ -3,37 +3,19 @@ package org.mod4j.businessdomain.generator.helpers;
 import java.util.ArrayList;
 import java.util.List;
 
-
-import org.mod4j.crossx.broker.CrossxBroker;
-import org.mod4j.dslcommon.generator.helpers.ProjectProperties;
-import org.mod4j.dslcommon.generator.helpers.StringHelpers;
-
-import BusinessDomainDsl.AbstractType;
-import BusinessDomainDsl.Association;
-import BusinessDomainDsl.BoolProperty;
-import BusinessDomainDsl.BusinessClass;
-import BusinessDomainDsl.AbstractBusinessClass;
-import BusinessDomainDsl.BusinessDomainModel;
-import BusinessDomainDsl.DateTimeProperty;
-import BusinessDomainDsl.DecimalProperty;
-import BusinessDomainDsl.Enumeration;
-import BusinessDomainDsl.EnumerationProperty;
-import BusinessDomainDsl.IntegerProperty;
-import BusinessDomainDsl.Multiplicity;
-import BusinessDomainDsl.Property;
-import BusinessDomainDsl.StringProperty;
+import org.mod4j.crossx.broker.CrossxEnvironment;
 
 public class CrossxHelpers {
 
-    public static List<String> allBusinessClasses() {
-    	List<String> result = new ArrayList<String>();
-    	List<String> tmp = CrossxBroker.findAll("BusinessClass");
-    	if( tmp != null ) {
-    		System.err.println("CROSSX HELPER FINDALL [" + tmp.toString() + "]");
-    		result.addAll(tmp);
-    	} else {
-    		System.err.println("CROSSX HELPER FINDALL NULL");
-    	}
-    	return result;
+    public static List<String> allBusinessClasses(String project) {
+        List<String> result = new ArrayList<String>();
+        List<String> tmp = CrossxEnvironment.findAll(project, "BusinessClass");
+        if (tmp != null) {
+            System.err.println("CROSSX HELPER FINDALL [" + tmp.toString() + "]");
+            result.addAll(tmp);
+        } else {
+            System.err.println("CROSSX HELPER FINDALL NULL");
+        }
+        return result;
     }
 }
