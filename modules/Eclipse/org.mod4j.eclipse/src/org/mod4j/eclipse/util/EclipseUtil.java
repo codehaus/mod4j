a2b13161959a33ea0f8e5a4ae54764e3462dab5a
jwa11799 <jwa11799@86619b79-1f46-0410-a122-eb9e176381dc>
2008-09-15 15:32:10 +0000

    Quick fix to include dataconract dsl

--- modules/Eclipse/org.mod4j.eclipse/src/org/mod4j/eclipse/util/EclipseUtil.java
+++ modules/Eclipse/org.mod4j.eclipse/src/org/mod4j/eclipse/util/EclipseUtil.java
@@ -83,7 +83,12 @@ public class EclipseUtil {
 	static public IPath getPath(String bundlename, String pathname) {
 		String myPathname = null;
 		if( Platform.inDevelopmentMode() ){ 
-			myPathname = "src/main/oaw/"+pathname;
+            if( bundlename.equals("org.mod4j.dsl.businessdomain.generator")) {
+                myPathname = "src/main/templates/"+pathname;
+            }
+            if( bundlename.equals("org.mod4j.dsl.datacontract.generator")) {
+                myPathname = "src/main/oaw/"+pathname;
+            }
 		} else {
 			myPathname = pathname;
 		}
