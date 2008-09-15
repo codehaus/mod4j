c79be8aa39082fe259722c91479d4a34818d93cf
johan <johan@86619b79-1f46-0410-a122-eb9e176381dc>
2008-09-15 13:35:52 +0000

    formatted

--- modules/org.mod4j.common/src/main/java/org/mod4j/common/generator/admin/FileTracker.java
+++ modules/org.mod4j.common/src/main/java/org/mod4j/common/generator/admin/FileTracker.java
@@ -6,13 +6,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 public class FileTracker {
-    
+
     static private FileTracker fileTracker = null;
-    
+
     static public FileTracker getFileTracker() {
-        if( fileTracker == null ) {
+        if (fileTracker == null) {
             fileTracker = new FileTracker();
         }
         return fileTracker;
@@ -21,22 +20,24 @@ public class FileTracker {
     private FileTracker() {
         track = new HashMap<String, FileTrack>();
     }
-    
-	private Map<String, FileTrack> track = new HashMap<String, FileTrack>();
-	private FileTrack current = null;
-	
-    /** Notify the ExtensionPointtracker that 'resource'is started
+
+    private Map<String, FileTrack> track = new HashMap<String, FileTrack>();
+
+    private FileTrack current = null;
+
+    /**
+     * Notify the ExtensionPointtracker that 'resource'is started
      * 
      * @param resource
      */
-	public void initResource(String resource) {
-		System.err.println("FileTracker resource [" + resource + "]");
-		current = getTrack(resource);
-	}
-	
-	private FileTrack getTrack(String resource) {
+    public void initResource(String resource) {
+        System.err.println("FileTracker resource [" + resource + "]");
+        current = getTrack(resource);
+    }
+
+    private FileTrack getTrack(String resource) {
         FileTrack result = track.get(resource);
-        if( result == null ){
+        if (result == null) {
             result = new FileTrack(resource);
             track.put(resource, result);
         } else {
@@ -44,34 +45,34 @@ public class FileTracker {
         }
         return result;
     }
-	
-	public Collection<FileTrack> getTracks() {
-	    return Collections.unmodifiableCollection(track.values());
-	}
 
-    static public String generate(String filename){
+    public Collection<FileTrack> getTracks() {
+        return Collections.unmodifiableCollection(track.values());
+    }
+
+    static public String generate(String filename) {
         getFileTracker().current.generatedFile(filename);
         System.err.println("        FileTracker generate [" + filename + "]");
         return filename;
     }
-    
-    static public String extend(String filename){
+
+    static public String extend(String filename) {
         getFileTracker().current.extensionFile(filename);
         System.err.println("        FileTracker extension [" + filename + "]");
         return filename;
     }
-    
-    public List<String> getExtensionFiles(String resource){
+
+    public List<String> getExtensionFiles(String resource) {
         return track.get(resource).getExtensionFiles();
     }
 
-    public List<String> getGeneratedFiles(String resource){
+    public List<String> getGeneratedFiles(String resource) {
         return track.get(resource).getGeneratedFiles();
     }
-	
-//	static public void cleanAll() {
-//		currentFiles = null;
-//		currentResource = null;
-//		track = new HashMap<String, List<String>>();
-//	}
+
+    // static public void cleanAll() {
+    // currentFiles = null;
+    // currentResource = null;
+    // track = new HashMap<String, List<String>>();
+    // }
 }
