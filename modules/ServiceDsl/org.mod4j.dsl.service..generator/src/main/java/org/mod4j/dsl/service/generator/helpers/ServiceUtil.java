e16ac4c5f76596d10c144ad2e8a9bedb6ec03034
jwa11799 <jwa11799@86619b79-1f46-0410-a122-eb9e176381dc>
2008-09-28 13:10:48 +0000

    Code generation for service model

--- modules/ServiceDsl/org.mod4j.dsl.service..generator/src/main/java/org/mod4j/dsl/service/generator/helpers/ServiceUtil.java
+++ modules/ServiceDsl/org.mod4j.dsl.service..generator/src/main/java/org/mod4j/dsl/service/generator/helpers/ServiceUtil.java
@@ -1,9 +1,17 @@
 package org.mod4j.dsl.service.generator.helpers;
 
 import org.mod4j.dsl.service.mm.ServiceDsl.ServiceMethod;
+import org.mod4j.dslcommon.generator.helpers.ProjectProperties;
 import org.mod4j.dslcommon.generator.helpers.StringHelpers;
 
 public class ServiceUtil {
 
+    static public String javaClassName(String className){
+        return className;
+    }
+
+    static public String javaBaseClassName(String className){
+        return javaClassName(className) + ProjectProperties.IMPLBASE_SUFFIX;
+    }
    
  }
