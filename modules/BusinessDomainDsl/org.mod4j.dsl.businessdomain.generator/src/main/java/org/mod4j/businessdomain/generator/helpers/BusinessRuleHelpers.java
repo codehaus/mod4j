84fb8b15af6e88a5fdc20117c804b1b35376aa05
jwa11799 <jwa11799@86619b79-1f46-0410-a122-eb9e176381dc>
2008-10-20 11:52:21 +0000

    Use new extend.generation scheme to enable tracking of generated files

--- modules/BusinessDomainDsl/org.mod4j.dsl.businessdomain.generator/src/main/java/org/mod4j/businessdomain/generator/helpers/BusinessRuleHelpers.java
+++ modules/BusinessDomainDsl/org.mod4j.dsl.businessdomain.generator/src/main/java/org/mod4j/businessdomain/generator/helpers/BusinessRuleHelpers.java
@@ -11,28 +11,28 @@ public class BusinessRuleHelpers {
      * @param br
      * @return The name of the Java class for br
      */
-    public static String javaClassName(BusinessRule br) {
-        return StringHelpers.firstCharToUpper(br.getName());
-    }
+//    public static String javaClassName(BusinessRule br) {
+//        return StringHelpers.firstCharToUpper(br.getName());
+//    }
 
     /**
      * @param br
      * @return The name of the abstract implementation base class for BusinessRule br
      */
-    public static String implBaseClassName(BusinessRule br) {
-        return javaClassName(br) + ProjectProperties.IMPLBASE_SUFFIX;
-    }
+//    public static String implBaseClassName(BusinessRule br) {
+//        return javaClassName(br) + ProjectProperties.IMPLBASE_SUFFIX;
+//    }
 
     /**
      * 
      * @return The full packagename of business rules
      */
-    public static String getBusinessRulesPackage() {
-        return ProjectProperties.getDomainRootPackage() + "." + ProjectProperties.BUSINESSRULES_PACKAGE;
-    }
+//    public static String getBusinessRulesPackage() {
+//        return ProjectProperties.getDomainRootPackage() + "." + ProjectProperties.BUSINESSRULES_PACKAGE;
+//    }
     
-    public static String getBusinessRulesPackageAsPath() {
-        return getBusinessRulesPackage().replaceAll("\\.", "/");
-    }
+//    public static String getBusinessRulesPackageAsPath() {
+//        return getBusinessRulesPackage().replaceAll("\\.", "/");
+//    }
 
 }
