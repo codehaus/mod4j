89f8b7c4d7d3f2a0244d4f61dda08665354ef379
jwa11799 <jwa11799@86619b79-1f46-0410-a122-eb9e176381dc>
2008-11-26 14:50:26 +0000

    Cleaned up to enable separation of CrossxBuilder

--- modules/ServiceDsl/org.mod4j.dsl.service.xtext.editor/src/org/mod4j/dsl/service/xtext/ServiceDslEditorPlugin.java
+++ modules/ServiceDsl/org.mod4j.dsl.service.xtext.editor/src/org/mod4j/dsl/service/xtext/ServiceDslEditorPlugin.java
@@ -1,6 +1,7 @@
 package org.mod4j.dsl.service.xtext;
 
-import org.mod4j.eclipse.builder.CrossxBuilder;
+
+import org.mod4j.eclipse.builder.Mod4jBuilder;
 import org.openarchitectureware.xtext.AbstractXtextEditorPlugin;
 import org.openarchitectureware.xtext.LanguageUtilities;
 import org.osgi.framework.BundleContext;
@@ -27,7 +28,7 @@ public class ServiceDslEditorPlugin extends AbstractXtextEditorPlugin {
 
    protected void refreshPluginActions() {
        super.refreshPluginActions();
-       CrossxBuilder.initCrossx(); 
+       Mod4jBuilder.initCrossx(); 
    }
 
 }
