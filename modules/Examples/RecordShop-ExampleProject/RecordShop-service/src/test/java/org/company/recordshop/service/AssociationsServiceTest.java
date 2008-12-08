6d1f4fecbd0afac35a81eff4f2873260ff1d47da
jwa11799 <jwa11799@86619b79-1f46-0410-a122-eb9e176381dc>
2008-12-08 13:08:58 +0000

    Added test to check for service exception when using incorrect parameters

--- modules/Examples/RecordShop-ExampleProject/RecordShop-service/src/test/java/org/company/recordshop/service/AssociationsServiceTest.java
+++ modules/Examples/RecordShop-ExampleProject/RecordShop-service/src/test/java/org/company/recordshop/service/AssociationsServiceTest.java
@@ -1,8 +1,12 @@
 package org.company.recordshop.service;
 
 import static org.junit.Assert.fail;
+
+import java.util.Set;
+
 import org.junit.Assert;
 
+import org.apache.derby.iapi.types.Orderable;
 import org.company.recordshop.service.dto.OrderNumberAndDateDto;
 import org.company.recordshop.service.dto.SimpleCustomerDto;
 import org.company.recordshop.service.dto.OrderDto;
@@ -64,6 +68,82 @@ public class AssociationsServiceTest extends
 		Assert.assertTrue(o.getCustomer().getOrders().size() == 1);
 		tearDown();
 	}
+	
+	@Test
+	public final void testAddOrderFailure() {
+		setup();
+
+		Long id = createdOrder.getId();
+		try {
+			customerServiceModelService.addToOrders(null, createdCustomer);
+			Assert.fail("Expecting Service Exception: Adding null order to customer");
+		} catch( ServiceException e) {
+			System.err.println("Expected exception: "+ e.getMessage());
+		}
+
+		try {
+			customerServiceModelService.addToOrders(createdOrder, null);
+			Assert.fail("Expecting Service Exception: Adding order to null customer");
+		} catch( ServiceException e) {
+			System.err.println("Expected exception: "+ e.getMessage());
+		}
+		
+		try {
+			createdOrder.setId(-1L);
+			customerServiceModelService.addToOrders(createdOrder, createdCustomer);
+			Assert.fail("Expecting Service Exception: Adding order with incorrect id to customer");
+		} catch( ServiceException e) {
+			System.err.println("Expected exception: "+ e.getMessage());
+		}
+		
+		try {
+			createdOrder.setId(id);
+			createdCustomer.setId(-1L);
+			customerServiceModelService.addToOrders(createdOrder, createdCustomer);
+			Assert.fail("Expecting Service Exception: Adding order to customer with incorrect id");
+		} catch( ServiceException e) {
+			System.err.println("Expected exception: "+ e.getMessage());
+		}
+		tearDown();
+	}
+	
+	@Test
+	public final void testAddCustomer() {
+		setup();
+
+		Long id = createdOrder.getId();
+		customerServiceModelService.setCustomer(createdCustomer, createdOrder);
+
+		OrderNumberAndDateDto o = orderServiceModelService
+				.readOrderAsOrderNumberAndDateDto(createdOrder.getId());
+		SimpleCustomerDto orderCustomer = o.getCustomer();
+		Assert.assertEquals(o.getCustomer().getId(), createdCustomer.getId());
+		
+		Set<OrderNumberAndDateDto> orders = o.getCustomer().getOrders();
+		Assert.assertEquals( ((OrderNumberAndDateDto)(orders.toArray()[0])).getId(), id);
+		
+		customerServiceModelService.setCustomer(null, createdOrder);
+		o = orderServiceModelService.readOrderAsOrderNumberAndDateDto(createdOrder.getId());
+		Assert.assertNull(o.getCustomer());
+		
+		SimpleCustomerDto sim = customerServiceModelService.readCustomer(createdCustomer.getId());
+		Assert.assertTrue( sim.getOrders().size() == 0);
+		tearDown();
+	}
+
+	@Test
+	public final void testAddCustomerFailure() {
+		setup();
+
+		try {
+			customerServiceModelService.setCustomer(createdCustomer, null);
+			Assert.fail("Expecting Service Exception: Adding customer to null order");
+		} catch( ServiceException e) {
+			System.err.println("Expected exception: "+ e.getMessage());
+		}
+
+		tearDown();
+	}
 
 	@Test
 	public final void testRemoveOrder() {
@@ -78,6 +158,32 @@ public class AssociationsServiceTest extends
 		SimpleCustomerDto orderCustomer2 = o2.getCustomer();
 		System.err.println("orderCustomer2 [" + orderCustomer2 + "]");
 		Assert.assertEquals(orderCustomer2, null);
+		
+		tearDown();
+	}
+	
+	@Test
+	public final void testRemoveOrderFailure() {
+		setup();
+
+		customerServiceModelService.addToOrders(createdOrder, createdCustomer);
+		OrderDto newOrder = orderServiceModelService.readOrderAsOrderDto(createdOrder.getId());
+
+		try {
+			customerServiceModelService.removeFromOrders(newOrder, null);
+			Assert.fail("Expecting Service Exception: Removing order from null customer");
+		} catch( ServiceException e) {
+			System.err.println("Expected exception: "+ e.getMessage());
+		}
+
+		try {
+			customerServiceModelService.removeFromOrders(null, createdCustomer);
+			Assert.fail("Expecting Service Exception: Removing null order from customer");
+		} catch( ServiceException e) {
+			System.err.println("Expected exception: "+ e.getMessage());
+		}
+		
+		tearDown();
 	}
 
 }
