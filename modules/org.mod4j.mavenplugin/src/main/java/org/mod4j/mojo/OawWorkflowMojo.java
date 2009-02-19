d407a7860ac22c31160d6115071b3eb94c1c02d8
jwarmer <jwarmer@86619b79-1f46-0410-a122-eb9e176381dc>
2009-02-19 13:29:47 +0000

    Added standard comment with copyright and reference to the Eclipse Public License

--- modules/org.mod4j.mavenplugin/src/main/java/org/mod4j/mojo/OawWorkflowMojo.java
+++ modules/org.mod4j.mavenplugin/src/main/java/org/mod4j/mojo/OawWorkflowMojo.java
@@ -11,6 +11,8 @@
  * 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * 	See the License for the specific language governing permissions and
  * 	limitations under the License.
+ * 
+ *  Adapted by Mod4j
  */
 package org.mod4j.mojo;
 
