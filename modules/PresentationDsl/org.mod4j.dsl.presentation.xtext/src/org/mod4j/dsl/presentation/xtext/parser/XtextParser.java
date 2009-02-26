697703945abc88237a8a60901ac46a2ee40740b4
jimvandam <jimvandam@86619b79-1f46-0410-a122-eb9e176381dc>
2009-02-26 10:19:45 +0000

    initial commit

---
+++ modules/PresentationDsl/org.mod4j.dsl.presentation.xtext/src/org/mod4j/dsl/presentation/xtext/parser/XtextParser.java
@@ -0,0 +1,14 @@
+
+package org.mod4j.dsl.presentation.xtext.parser;
+
+import java.io.InputStream;
+
+import org.mod4j.dsl.presentation.xtext.parser.GenParser;
+
+public class XtextParser extends GenParser {
+
+	public XtextParser(InputStream in) {
+		super(in);
+	}
+
+}
