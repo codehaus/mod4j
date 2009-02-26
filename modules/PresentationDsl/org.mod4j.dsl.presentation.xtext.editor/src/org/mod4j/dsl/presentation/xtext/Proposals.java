2373c3ebdb6c86b390ee8a2251e9753fa9d6f78a
jimvandam <jimvandam@86619b79-1f46-0410-a122-eb9e176381dc>
2009-02-26 15:18:06 +0000

    improved contentassist for DialogueCalls

--- modules/PresentationDsl/org.mod4j.dsl.presentation.xtext.editor/src/org/mod4j/dsl/presentation/xtext/Proposals.java
+++ modules/PresentationDsl/org.mod4j.dsl.presentation.xtext.editor/src/org/mod4j/dsl/presentation/xtext/Proposals.java
@@ -1,22 +1,25 @@
 package org.mod4j.dsl.presentation.xtext;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
 import org.mod4j.crossx.broker.CrossxBroker;
 import org.mod4j.crossx.mm.crossx.Symbol;
+import org.mod4j.dsl.presentation.mm.PresentationDsl.Dialogue;
+import org.mod4j.dsl.presentation.mm.PresentationDsl.DialogueCall;
 import org.mod4j.dsl.presentation.mm.PresentationDsl.DtoPropertyReference;
 import org.mod4j.dsl.presentation.mm.PresentationDsl.ExternalReference;
 import org.mod4j.dsl.presentation.mm.PresentationDsl.FormElement;
+import org.mod4j.dsl.presentation.mm.PresentationDsl.ModelElement;
 
 public class Proposals {
+	
 	   /**
-     * Get a list of proposals for a BusinessClassReference for context 'ctx'
+     * Get a list of proposals for a FormElementReference for context 'ctx'
      * 
      * @param ctx
-     *            The conmtext within xText for which the proposals is created
+     *            The context within xText for which the proposals is created
      * @return
      */
     static public List<String> getFormElementReferenceProposals(EObject ctx) {
@@ -41,6 +44,51 @@ public class Proposals {
         return result;
     }
 
+	   /**
+     * Get a list of proposals for a DialogueCall for context 'ctx'
+     * 
+     * @param ctx
+     *            The context within xText for which the proposals is created
+     * @return
+     */
+    static public List<String> getDialogueCallProposals(EObject ctx) {
+        List<String> result = new ArrayList<String>();
+        ExternalReference dtoRef = null;
+        if (ctx == null) {
+            return result;
+        }
+        List<String> models = new ArrayList<String>();
+        // The context may be of different types
+        if (ctx instanceof DialogueCall) {
+        	ctx = ctx.eContainer();
+        }
+        if (ctx instanceof Dialogue) {
+        	Dialogue d = (Dialogue) ctx;
+        	for (ModelElement element : d.getPresentationModel().getElements()) {
+        		if (element instanceof Dialogue && d.getContextRef().getName().equals(((Dialogue)element).getContextRef().getName())) {
+        			result.add(element.getName());
+        		}
+        	}
+            dtoRef = d.getContextRef();
+            List<ExternalReference> refs = d.getPresentationModel().getExternalReferences();
+            for (ExternalReference ref : refs) {
+            	models.add(ref.getModelName());
+            }
+        }
+        if (dtoRef == null) {
+            result.add("<<no context found>>");
+            return result;
+        }
+        for (String model : models) {
+	        List<Symbol> symbols = CrossxBroker.findAllFromModel(model, "Dialogue");
+	        for (Symbol symb : symbols) {
+	        	if (dtoRef.getName().equals(CrossxBroker.getPropertyValue(symb, "context")))
+	        	result.add(symb.getName());
+	        }
+        }
+        return result;
+    }
+    
     /**
      * Checks whether the list 'properties' contains an element with name 'name'
      * @param properties
