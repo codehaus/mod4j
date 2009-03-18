1dc400f88966db945f3f562f2e8e5345519370c4
jwarmer <jwarmer@86619b79-1f46-0410-a122-eb9e176381dc>
2009-03-18 14:27:42 +0000

    Added ordered to metamodel for supporting ordered associations at the datacontract level.

---
+++ modules/DataContractDsl/org.mod4j.dsl.datacontract.mm/src/org/mod4j/dsl/datacontract/mm/DataContractDsl/DtoEnumerationProperty.java
@@ -0,0 +1,53 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.mod4j.dsl.datacontract.mm.DataContractDsl;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Dto Enumeration Property</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link org.mod4j.dsl.datacontract.mm.DataContractDsl.DtoEnumerationProperty#getType <em>Type</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see org.mod4j.dsl.datacontract.mm.DataContractDsl.DataContractDslPackage#getDtoEnumerationProperty()
+ * @model
+ * @generated
+ */
+public interface DtoEnumerationProperty extends DtoDataProperty {
+	/**
+	 * Returns the value of the '<em><b>Type</b></em>' reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Type</em>' reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Type</em>' reference.
+	 * @see #setType(EnumerationDto)
+	 * @see org.mod4j.dsl.datacontract.mm.DataContractDsl.DataContractDslPackage#getDtoEnumerationProperty_Type()
+	 * @model
+	 * @generated
+	 */
+	EnumerationDto getType();
+
+	/**
+	 * Sets the value of the '{@link org.mod4j.dsl.datacontract.mm.DataContractDsl.DtoEnumerationProperty#getType <em>Type</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Type</em>' reference.
+	 * @see #getType()
+	 * @generated
+	 */
+	void setType(EnumerationDto value);
+
+} // DtoEnumerationProperty
---
+++ modules/DataContractDsl/org.mod4j.dsl.datacontract.mm/src/org/mod4j/dsl/datacontract/mm/DataContractDsl/impl/DtoDateTimePropertyImpl.java
@@ -0,0 +1,43 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.mod4j.dsl.datacontract.mm.DataContractDsl.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.mod4j.dsl.datacontract.mm.DataContractDsl.DataContractDslPackage;
+import org.mod4j.dsl.datacontract.mm.DataContractDsl.DtoDateTimeProperty;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Dto Date Time Property</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class DtoDateTimePropertyImpl extends DtoDataPropertyImpl implements DtoDateTimeProperty {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected DtoDateTimePropertyImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return DataContractDslPackage.Literals.DTO_DATE_TIME_PROPERTY;
+	}
+
+} //DtoDateTimePropertyImpl
