16249e746e135ba7916b6379a077cf36660e1543
boxtelp <boxtelp@86619b79-1f46-0410-a122-eb9e176381dc>
2009-04-07 10:43:12 +0000

    look & feel modifications

--- demos/breakfast/breakfast-web/src/main/java/com/rosa/breakfast/web/page/BasePage.java
+++ demos/breakfast/breakfast-web/src/main/java/com/rosa/breakfast/web/page/BasePage.java
@@ -6,8 +6,14 @@ import org.apache.wicket.markup.html.resources.StyleSheetReference;
 public abstract class BasePage extends WebPage {
 
 	public BasePage() {
-		add(new StyleSheetReference("stylesheet", BaseAppPage.class,
-				"../stylesheet/breakfast.css"));
+		// add(new StyleSheetReference("stylesheet", BaseAppPage.class,
+		// "../stylesheet/breakfast.css"));
+		add(new StyleSheetReference("stylesheet1", BaseAppPage.class,
+				"../stylesheet/maven-base.css"));
+		add(new StyleSheetReference("stylesheet2", BaseAppPage.class,
+				"../stylesheet/maven-theme.css"));
+		add(new StyleSheetReference("stylesheet3", BaseAppPage.class,
+				"../stylesheet/site.css"));
 	}
 
 }
