1495e8ec4ac331127ea1d1c89d8ab5ca6a3203a3
prins <prins@86619b79-1f46-0410-a122-eb9e176381dc>
2009-04-09 12:40:10 +0000

    - initial load script

--- demos/breakfast/breakfast-data/src/main/sql/initial load.sql
+++ demos/breakfast/breakfast-data/src/main/sql/initial load.sql
@@ -14,23 +14,23 @@ INSERT INTO standardbreakfast_table ( id, version, name, price, style ) VALUES (
 INSERT INTO standardbreakfast_table ( id, version, name, price, style ) VALUES ( 102, 0, 'Ghanaian', 6.95, 1 );
 
 -- comestibles
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 200, 0, 'Coffee', 1, 1.0, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 201, 0, 'Croissant', 1, 0.50, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 202, 0, 'Bread slice', 1, 0.20, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 203, 0, 'Orange juice', 1, 0.50, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 204, 0, 'Sausage', 1, 1.00, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 205, 0, 'Egg', 2, 1.00, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 206, 0, 'Toast', 1, 0.30, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 207, 0, 'Beans', 1, 1.50, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 208, 0, 'Tomato slice', 2, 0.10, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 209, 0, 'Jam', 1, 0.25, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 210, 0, 'Ham slice', 1, 0.15, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 211, 0, 'Cheese slice', 1, 0.25, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 212, 0, 'Butter', 1, 0.05, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 213, 0, 'Tea', 1, 0.25, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 214, 0, 'Goat omelet', 1, 1.75, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 215, 0, 'Sugar bread', 2, 1.25, null );
-INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 216, 0, 'Tomato juice', 2, 1.00, null );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 200, 0, 'Coffee', 1, 1.0, 'Mug' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 201, 0, 'Croissant', 1, 0.50, 'Plate' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 202, 0, 'Bread slice', 1, 0.20, 'Plate' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 203, 0, 'Orange juice', 1, 0.50, 'Glass' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 204, 0, 'Sausage', 1, 1.00, 'Plate' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 205, 0, 'Egg', 2, 1.00, 'Cup' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 206, 0, 'Toast', 1, 0.30, 'Basket' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 207, 0, 'Beans', 1, 1.50, 'Plate' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 208, 0, 'Tomato slice', 2, 0.10, 'Plate' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 209, 0, 'Jam', 1, 0.25, 'Cup' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 210, 0, 'Ham slice', 1, 0.15, 'Plate' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 211, 0, 'Cheese slice', 1, 0.25, 'Plate' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 212, 0, 'Butter', 1, 0.05, 'Cup' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 213, 0, 'Tea', 1, 0.25, 'Glass' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 214, 0, 'Goat omelet', 1, 1.75, 'Plate' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 215, 0, 'Sugar bread', 2, 1.25, 'Basket' );
+INSERT INTO comestible_table ( id, version, name, minimalquantity, price, transportform ) VALUES ( 216, 0, 'Tomato juice', 2, 1.00, 'Glass' );
 
 -- parts
 -- Continental breakfast
