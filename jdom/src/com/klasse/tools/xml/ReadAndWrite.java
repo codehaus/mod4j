5d124c094a77b23a7fe78f6c8e72e61dfd3840cb
jwarmer <jwarmer@86619b79-1f46-0410-a122-eb9e176381dc>
2009-07-23 10:05:37 +0000

    Moved to Galileo

---
+++ jdom/src/com/klasse/tools/xml/ReadAndWrite.java
@@ -0,0 +1,22 @@
+/*
+ * Created on Nov 16, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.klasse.tools.xml;
+
+/**
+ * @author jos
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class ReadAndWrite {
+
+	public ReadAndWrite(){
+		
+	}
+	
+	int aap = 0;
+}
