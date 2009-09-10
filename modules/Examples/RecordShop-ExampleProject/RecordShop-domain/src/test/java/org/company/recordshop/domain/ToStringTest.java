d272e9944b6031d4148b61fee0d11c83eab89352
johan <johan@86619b79-1f46-0410-a122-eb9e176381dc>
2009-09-10 11:44:48 +0000

    Adjusted order of toString

--- modules/Examples/RecordShop-ExampleProject/RecordShop-domain/src/test/java/org/company/recordshop/domain/ToStringTest.java
+++ modules/Examples/RecordShop-ExampleProject/RecordShop-domain/src/test/java/org/company/recordshop/domain/ToStringTest.java
@@ -7,21 +7,27 @@ import org.junit.Test;
 
 public class ToStringTest {
 
-    @Test
-    public void testToString() {
-        Customer customer = new Customer("Ludwig", "Wittgenstein", new DateTime(0L), 1);
-        customer.setDiscountPercentage(17);
-        customer.setEmailAddress("Ludwig.Wittgenstein@mod4j.org");
-        customer.setNumberOfEars(3);
-        customer.setSexe(SexeEnum.MALE);
-        customer.setUsername("lwittgen");
-        assertEquals(
-                "org.company.recordshop.domain.Customer[id=null,firstName=Ludwig," +
-                "lastName=Wittgenstein,numberOfEars=3,sexe=MALE," +
-                "birthDate=1970-01-01T01:00:00.000+01:00,customerNr=1," +
-                "username=lwittgen,emailAddress=Ludwig.Wittgenstein@mod4j.org," +
-                "blackListed=false,discountPercentage=17]",
-                customer.toString());
-    }
+	@Test
+	public void testToString() {
+		Customer customer = new Customer("Ludwig", "Wittgenstein",
+				new DateTime(0L), 1);
+		customer.setDiscountPercentage(17);
+		customer.setEmailAddress("Ludwig.Wittgenstein@mod4j.org");
+		customer.setNumberOfEars(3);
+		customer.setSexe(SexeEnum.MALE);
+		customer.setUsername("lwittgen");
+		assertEquals(
+				"org.company.recordshop.domain.Customer[id=null," +
+				"firstName=Ludwig," +
+				"lastName=Wittgenstein," +
+				"birthDate=1970-01-01T01:00:00.000+01:00," +
+				"numberOfEars=3" +
+				",sexe=MALE," +
+				"customerNr=1," +
+				"username=lwittgen," +
+				"emailAddress=Ludwig.Wittgenstein@mod4j.org," +
+				"blackListed=false,discountPercentage=17]",
+				customer.toString());
+	}
 
 }
